90e5022496bf0474fd9d02cbb78de90b4cc49556
Sebastien LEPERS <seb@meliweb.fr>
2017-05-24 16:00:43 +0200

    Products: Removed hardcoded entity type id

--- app/code/community/Pimgento/Product/Model/Import.php
+++ app/code/community/Pimgento/Product/Model/Import.php
@@ -14,6 +14,11 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
     protected $_code = 'product';
 
     /**
+     * @var int
+     */
+    protected $_productEntityTypeId = null;
+
+    /**
      * Create table (Step 1)
      *
      * @param Pimgento_Core_Model_Task $task
@@ -499,7 +504,7 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
 
         $values = array(
             'entity_id'        => 'entity_id',
-            'entity_type_id'   => $this->_zde(4),
+            'entity_type_id'   => $this->_zde($this->getDefaultAttributSetId()),
             'attribute_set_id' => 'family',
             'type_id'          => '_type_id',
             'sku'              => 'code',
@@ -682,17 +687,17 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
 
         $query = $adapter->query(
             $adapter->select()
-            ->from(
-                $this->getTable(),
-                array(
-                    'entity_id',
-                    'family',
-                    '_attributes',
-                    '_children'
+                ->from(
+                    $this->getTable(),
+                    array(
+                        'entity_id',
+                        'family',
+                        '_attributes',
+                        '_children'
+                    )
                 )
-            )
-            ->where('_type_id = ?', 'configurable')
-            ->where('_attributes <> ?', $this->_zde('""'))
+                ->where('_type_id = ?', 'configurable')
+                ->where('_attributes <> ?', $this->_zde('""'))
         );
 
         $stores = Mage::app()->getStores();
@@ -757,14 +762,14 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
 
                     $childId = $adapter->fetchOne(
                         $adapter->select()
-                        ->from(
-                            $resource->getTable('catalog/product'),
-                            array(
-                                'entity_id'
+                            ->from(
+                                $resource->getTable('catalog/product'),
+                                array(
+                                    'entity_id'
+                                )
                             )
-                        )
-                        ->where('sku = ?', $child)
-                        ->limit(1)
+                            ->where('sku = ?', $child)
+                            ->limit(1)
                     );
 
                     if ($childId) {
@@ -965,14 +970,14 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
         $priceId = $adapter->fetchOne(
             $adapter->select()
                 ->from($resource->getTable('eav/attribute'), array('attribute_id'))
-                ->where('entity_type_id = ?', 4)
+                ->where('entity_type_id = ?', $this->getDefaultAttributSetId())
                 ->where('attribute_code = ?', 'price')
                 ->limit(1));
 
         $specialPriceId = $adapter->fetchOne(
             $adapter->select()
                 ->from($resource->getTable('eav/attribute'), array('attribute_id'))
-                ->where('entity_type_id = ?', 4)
+                ->where('entity_type_id = ?', $this->getDefaultAttributSetId())
                 ->where('attribute_code = ?', 'special_price')
                 ->limit(1));
 
@@ -1348,8 +1353,8 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
         );
 
         $assetSqlColumns = implode(',', array_map(function($asset) {
-                    return '`p`.`' . $asset . '`';
-                }, $assetAkeneoAttributeCodes));
+            return '`p`.`' . $asset . '`';
+        }, $assetAkeneoAttributeCodes));
 
         $joiningCondition = 'FIND_IN_SET(`a`.`asset`, CONCAT_WS(",", '. $assetSqlColumns . '))';
 
@@ -1564,4 +1569,16 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
         return true;
     }
 
+    /**
+     * get product entity type id
+     *
+     * @return int
+     */
+    public function getProductEntityTypeId()
+    {
+        if ($this->_productEntityTypeId === NULL) {
+            $this->_productEntityTypeId = Mage::helper('pimgento_core')->getProductEntityTypeId();
+        }
+        return $this->_productEntityTypeId;
+    }
 }
