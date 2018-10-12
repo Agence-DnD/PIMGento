1f0b9a4c7a5cc1bcfafd3d18036ec28d7cd3ca02
Enzo Perrotta <ep@kendesign.it>
2018-10-12 16:00:04 +0200

    Fixed the class name for the VariantFamily Cron class

--- app/code/community/Pimgento/VariantFamily/Model/Cron.php
+++ app/code/community/Pimgento/VariantFamily/Model/Cron.php
@@ -5,7 +5,7 @@
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Pimgento_Variant_Model_Cron extends Pimgento_Core_Model_Cron
+class Pimgento_VariantFamily_Model_Cron extends Pimgento_Core_Model_Cron
 {
 
     /**
